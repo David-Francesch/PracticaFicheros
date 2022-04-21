61e5f705767a63274011ce804eed461a8b66737b
David-Francesch <david.francesch.colomer@gmail.com>
2022-04-21 20:40:40 +0200

    comienzo de algo

--- practicaficheros/PracticaFicheros/src/MainFicheros.java
+++ practicaficheros/PracticaFicheros/src/MainFicheros.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class MainFicheros {
     public static void main(String[] args) throws Exception {
-        
+        File fichero = new File("../../test.txt");
+        FileReader fr = new FileReader(fichero);
+        BufferedReader bfr = new BufferedReader(fr);
     }
+
 }
---
+++ practicaficheros/PracticaFicheros/src/VectorStrings.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class VectorStrings {
+    
+    
+
+    public VectorStrings(){
+        Vector test = new Vector();
+    }
+
+    public VectorStrings(String fich){
+
+    }
+
+    public void LeerFich() {
+        
+    }
+}
